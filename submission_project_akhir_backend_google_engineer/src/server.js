655f86763d722c3a7cf37df66b1bf1b4e821ef1f
hildanku <hildankutomo@gmail.com>
2024-10-31 14:00:09 +0700

    feat(server): added routes.js as endpoint routing

--- submission_project_akhir_backend_google_engineer/src/server.js
+++ submission_project_akhir_backend_google_engineer/src/server.js
@@ -1,12 +1,12 @@
 const Hapi = require('@hapi/hapi');
-
+const routes =  require('./routes');
 const init = async () => {
 
   const server = Hapi.server({
     port: 9000,
     host: 'localhost'
   });
-
+  server.route(routes)
   await server.start()
   console.log(`server running at port ${server.info.uri}`);
 }
