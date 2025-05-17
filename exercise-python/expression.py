a283568edfd5182ba41aa5b407324f1048fd086e
hildanku <hildankutomo@gmail.com>
2025-05-18 00:11:59 +0700

    feat: add expression and quiz

---
+++ exercise-python/expression.py
@@ -0,0 +1,67 @@
+x = 10
+y = 2
+result = x - y
+print(result)
+
+angka = [2, 4, 6, 8]
+huruf = ['P', 'Y', 'T', 'H', 'O', 'N']
+gabung = angka + huruf
+print(gabung)
+
+learn = ['P', 'Y', 'T', 'H', 'O', 'N']
+replikasi = learn * 2
+
+print(replikasi)
+
+# Jenis
+# # Biner
+# (x + y), (x - y), (x * y), (x / y), (x ** y),
+# (x < y), (x <= y), (x > y), (x >= y), (x % y), (x == y), (x != y).
+
+# Uner
+# (x += 1), (x-=1), (not x).
+x = 11
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+# +=
+x = 11
+x += 5
+print(x)
+
+# -=
+x = 11
+x -= 5
+print(x)
+
+# *=
+x = 11
+x *= 5
+print(x)
+
+# /=
+x = 11
+x /= 5
+print(x)
+
+# %=
+x = 11
+x %= 5
+print(x)
---
+++ exercise-python/q.py
@@ -0,0 +1,20 @@
+# x = 'Dicoding'
+# x[0] = 'F'
+# print(x)
+
+# x = (5, 'program', 1+3j)
+# x[1] = 'Dicoding'
+# print(x)
+
+# x = 1960
+# x += 901
+# print(x)
+
+# x = 10
+# y = 999
+# result = x > y
+# print(result)
+
+x = True
+y = False
+print(x or y)
