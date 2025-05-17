27dd24ee5a1e4af93e9d48ee7b1fa27b43ae9d5e
hildanku <hildankutomo@gmail.com>
2025-05-17 23:11:41 +0700

    feat: add list set & string

---
+++ exercise-python/list-set-string.py
@@ -0,0 +1,26 @@
+list = [1, 3, 3, 5, 5, 5, 7, 7, 9]
+print(list)
+print(len(list))
+
+list_set = set([1, 3, 3, 5, 5, 5, 7, 7, 9])
+print(list_set)
+print(len(list_set))
+
+# find min max
+num = [13, 7, 24, 5, 96, 84, 71, 11, 38]
+print(min(num))
+print(max(num))
+
+# find count
+num = [2, 4, 5, 6, 7, 7, 7, 8]
+print(num.count(7))
+
+# substring
+string = "Belajar Python di Dicoding sangat menyenangkan"
+substring = "a"
+print(string.count(substring))
+
+
+kendaraan = ['motor', 'mobil', 'helikopter', 'pesawat']
+kendaraan.sort()
+print(kendaraan)
