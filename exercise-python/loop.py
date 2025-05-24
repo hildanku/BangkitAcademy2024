baca0c429f668f0412cb6d1bd7b0796f2aa7c45c
hildanku <hildankutomo@gmail.com>
2025-05-24 19:58:08 +0700

    feat: add loop

---
+++ exercise-python/loop.py
@@ -0,0 +1,16 @@
+var_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in var_list:
+    print(i)
+
+for i in range(10):
+    print(i)
+# when use range(10) will print 0-9
+# range(start, stop, step)
+
+for i in range(1, 10, 2):
+    print(i)
+
+counter = 1
+while counter <= 5:
+    print(counter)
+    counter += 1    # Increment
