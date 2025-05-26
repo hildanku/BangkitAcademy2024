1ac2f406296a8ab711e7a485ca57c9778cba7d80
hildanku <hildankutomo@gmail.com>
2025-05-26 08:49:18 +0700

    feat: add sample how to write class

---
+++ exercise-python/oop/class-blueprint.py
@@ -0,0 +1,13 @@
+# class is blueprint for creating object ex: Animal, Car
+# object is instance of class ex: dog, hildan car
+# method is function in class
+# attruvbute is data in class
+
+class Mobil:
+    warna = "merah"
+
+
+# to call class
+mobil2 = Mobil()
+# print(objectname.warna)
+print(mobil2.warna)
