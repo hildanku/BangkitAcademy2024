6a899ce8b5bfcf3634ffafe49eba9390e06b96cf
hildanku <hildankutomo@gmail.com>
2025-05-27 21:38:34 +0700

    feat: init model & migrations for notes entity

--- exercise-python/drf-basic/drf_app/models.py
+++ exercise-python/drf-basic/drf_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class Note(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    tags = models.JSONField()
+    createdAt = models.DateTimeField(auto_now_add=True)
