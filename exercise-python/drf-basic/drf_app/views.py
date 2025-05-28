948c822f35571be6f72bea8be290ae36c3aa0490
hildanku <hildankutomo@gmail.com>
2025-05-28 08:31:34 +0700

    feat: add get by primary key method

--- exercise-python/drf-basic/drf_app/views.py
+++ exercise-python/drf-basic/drf_app/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from drf_app.serializers import NoteSerializer
 from drf_app.models import Note
-
+from django.http import Http404
 # Create your views here.
 #
 
@@ -26,3 +26,16 @@ class NoteList(APIView):
         return Response({
             "notes": serializer.data
         }, status=status.HTTP_200_OK)
+
+
+class NoteDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
