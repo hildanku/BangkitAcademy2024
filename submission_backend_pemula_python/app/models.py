4a88b088b73df9b8f4196409dc9949b021bcb769
hildanku <hildankutomo@gmail.com>
2025-05-29 16:51:55 +0700

    feat(bpdp/models): add Product models - generate migrations

--- submission_backend_pemula_python/app/models.py
+++ submission_backend_pemula_python/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    shop = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    price = models.IntegerField()
+    discount = models.IntegerField(default=0)
+    category = models.CharField(max_length=255)
+    stock = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    picture = models.CharField(max_length=255)
