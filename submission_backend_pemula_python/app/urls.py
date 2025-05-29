f1e83d329f28f088dab91f0fc93fb2431f90ad83
hildanku <hildankutomo@gmail.com>
2025-05-29 23:12:52 +0700

    feat(bpdp/views): add Product Detail feature - add class ProductDetail - add method get to get product detail - regist class to route urls

--- submission_backend_pemula_python/app/urls.py
+++ submission_backend_pemula_python/app/urls.py
@@ -3,4 +3,5 @@ from app import views
  
 urlpatterns = [
   path('products/',views.ProductList.as_view(), name='product-list'),
+  path('products/<uuid:pk>/', views.ProductDetail.as_view(), name='product-detail'),
 ]
--- submission_backend_pemula_python/app/views.py
+++ submission_backend_pemula_python/app/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from app.serializers import ProductSerializer
 from app.models import Product
+from django.http import Http404
 
 class ProductList(APIView):
     def post(self, request):
@@ -12,3 +13,27 @@ class ProductList(APIView):
             product.save()
             return Response(product.data, status=status.HTTP_201_CREATED)
         return Response(product.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        products = Product.objects.all()
+        # if products.is_empty():
+        #     #    return Response("Not Found", status=status.HTTP_404)
+        #     raise Http404
+        serializers = ProductSerializer(
+            products, many=True, context={'request': request})
+        return Response({
+            "products": serializers.data
+        }, status=status.HTTP_200_OK)
+
+
+class ProductDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
