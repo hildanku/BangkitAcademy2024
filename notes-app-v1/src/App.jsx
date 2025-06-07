99aa3db5eebdf2d63c0221754f95ce4cdfe53435
hildanku <hildankutomo@gmail.com>
2025-06-07 22:43:29 +0700

    feat: add not found page - implement strictmode & browserrouter - remove semicolon

--- notes-app-v1/src/App.jsx
+++ notes-app-v1/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
+import Header from './components/header'
+import NotFound from './pages/not-found'
 
 function App() {
-  return (
-    <div className="app-container">
-      <h1>Hello, React</h1>
-    </div>
-  );
+    return (
+        //<Header />
+        <NotFound />
+    )
 }
 
-export default App;
+export default App
--- notes-app-v1/src/index.jsx
+++ notes-app-v1/src/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
+import App from './App'
 
-import './styles/style.css';
+import './styles/style.css'
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'))
 root.render(
-  <App />,
-  );
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
