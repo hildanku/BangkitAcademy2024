a0e7c2a2844f43f187485234bf83d8e5cf7e97fb
hildanku <hildankutomo@gmail.com>
2025-06-08 23:55:25 +0700

    feat: add hamburger menu

--- notes-app-v1/src/components/navigation-bar.jsx
+++ notes-app-v1/src/components/navigation-bar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function NavigationBar() {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () => {
+        setIsOpen(prev => !prev)
+    }
+
     return (
-        <nav>
-            <Link to="/notes">Daftar Catatan</Link>
-            <Link to="/notes/new">Tambah</Link>
-        </nav>
+        <>
+            <button className="hamburger" onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <nav className={`navigation-bar ${isOpen ? 'open' : ''}`}>
+                <Link to="/">Daftar Catatan</Link>
+                <Link to="/notes/new">Tambah</Link>
+            </nav>
+        </>
     )
 }
 
