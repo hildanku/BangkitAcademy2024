b500f382f9c0d1017526e2c729ee12abd91e94c9
hildanku <hildankutomo@gmail.com>
2025-06-13 12:55:19 +0700

    feat: add script to start app oapi-v1 - remove unused project

--- openmusic-v1/src/validator/albums/album.schema.js
+++
@@ -1,8 +0,0 @@
-const joi = require('joi')
-
-const albumPayloadSchema = joi.object({
-    name: joi.string().required(),
-    year: joi.number().integer().required()
-})
-
-module.exports = { albumPayloadSchema }
