cf9f8db4800a5a2e5773335af68e48d05018e7e4
hildanku <hildankutomo@gmail.com>
2025-06-15 17:01:18 +0700

    feat: init table for likes_album and add columns cover to albums - setup rabbitmq compose

---
+++ oapi-v3/migrations/1749981382830_create-table-user-likes.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('user_likes_album', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        user_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+            references: 'users(id)',
+            onDelete: 'cascade',
+        },
+        album_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+            references: 'albums(id)',
+            onDelete: 'cascade',
+        },
+    })
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('user_likes_album')
+}
